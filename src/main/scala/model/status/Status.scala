97b8bc59cad96997bdc902df8022c74daa8ed5e9
Patrick Zwick <git@patri9ck.dev>
2025-01-17 15:38:37 +0100

    Added more documentation

--- src/main/scala/model/status/Status.scala
+++ src/main/scala/model/status/Status.scala
@@ -5,6 +5,19 @@ import play.api.libs.json.{Json, OFormat}
 
 import scala.xml.Elem
 
+/**
+ * Contains the current status of the game.
+ * @param players a list of all players, initially set to [[Nil]]
+ * @param stack a list of all cards, representing the stack, initially set to [[Nil]]
+ * @param trump an [[Option]] containing the trump. The trump is the last element on the [[stack]], initially set to [[None]].
+ * @param amount the amount of cards each player should have while the [[stack]] is non-empty, initially set to 0.
+ * @param turn the current turn, initially set [[model.Turn.Uninitialized]]
+ * @param defended a list of all currently defended cards, initially set to [[Nil]]
+ * @param undefended a list of all currently undefended cards, initially set to [[Nil]]
+ * @param used a list of all currently cards used to defend, initially set to [[Nil]]
+ * @param denied whether the player with the turn [[model.turn.FirstlyAttacking]] denied his attack, initially set to false
+ * @param passed the player who initially passed cards of the same rank to the next player, initially set to [[None]]
+ */
 case class Status(players: List[Player] = Nil,
                   stack: List[Card] = Nil,
                   trump: Option[Card] = None,
--- src/main/scala/view/tui/runner/SingleRunner.scala
+++ src/main/scala/view/tui/runner/SingleRunner.scala
@@ -2,6 +2,9 @@ package view.tui.runner
 
 import scala.io.StdIn
 
+/**
+ * A simple implementation of [[view.tui.runner.Runner]] reading simply from standard input single-threaded.
+ */
 class SingleRunner extends Runner {
 
   override def run(run: () => Unit): Unit = run.apply()
